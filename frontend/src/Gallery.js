7429f5ff672253f44479215a22ddaee449a9db23
Marius Obert <obatzer@gmail.com>
2022-10-29 23:36:11 +0200

    Add no data text

--- frontend/src/Gallery.js
+++ frontend/src/Gallery.js
@@ -2,19 +2,20 @@ import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import ImageList from "@material-ui/core/ImageList";
 import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 import ImageListItem from "@material-ui/core/ImageListItem";
 import ImageListItemBar from "@material-ui/core/ImageListItemBar";
-import Alert from '@material-ui/lab/Alert';
+import Alert from "@material-ui/lab/Alert";
 import { Skeleton } from "@material-ui/lab";
 import "react-image-lightbox/style.css";
 import Lightbox from "react-image-lightbox";
-import {format, register} from "timeago.js";
+import { format, register } from "timeago.js";
 import deLocal from "timeago.js/lib/lang/de";
 import useSWR from "swr";
 import useWindowDimensions from "./useWindowDimensions";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
-register("de", deLocal)
+register("de", deLocal);
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,7 +30,7 @@ const useStyles = makeStyles((theme) => ({
     position: "absolute",
     width: "95%",
     bottom: "10px",
-    padding: "10px"
+    padding: "10px",
   },
 }));
 
@@ -43,7 +44,7 @@ export default function Gallery() {
   if (error)
     return (
       <Alert variant="filled" severity="error" className={classes.alert}>
-       Es lief etwas schief :( 
+        Es lief etwas schief :(
       </Alert>
     );
 
@@ -51,43 +52,49 @@ export default function Gallery() {
 
   return (
     <div className={classes.root}>
-      <ImageList
-        variant="quilted"
-        cols={windowDimensions.width > windowDimensions.height ? 5 : 3}
-        rowHeight={200}
-      >
-        {data.map((media, index) =>
-          media ? (
-            <ImageListItem
-              key={`list-${index}`}
-              cols={1}
-              rows={1}
-              onClick={() => {
-                media.contentType.indexOf("video") && setIndex(index);
-              }}
-            >
-              {media.contentType.indexOf("video") ? (
-                <img src={media.src} alt={media.caption} loading="lazy" />
-              ) : (
-                <video autoPlay controls muted loop>
-                  <source src={media.src} type={media.contentType} />
-                  Your browser does not support the video tag.
-                </video>
-              )}
-              {media.caption && <ImageListItemBar title={media.caption} />}
-            </ImageListItem>
-          ) : (
-            <Box
-              key={`list-${index}`}
-              sx={{
-                boxSizing: "border-box",
-              }}
-            >
-              <Skeleton variant="rect" height="100%" />
-            </Box>
-          )
-        )}
-      </ImageList>
+      {data.length === 0 ? (
+        <Typography variant="subtitle1" align="center">
+          No images yet :)
+        </Typography>
+      ) : (
+        <ImageList
+          variant="quilted"
+          cols={windowDimensions.width > windowDimensions.height ? 5 : 3}
+          rowHeight={200}
+        >
+          {data.map((media, index) =>
+            media ? (
+              <ImageListItem
+                key={`list-${index}`}
+                cols={1}
+                rows={1}
+                onClick={() => {
+                  media.contentType.indexOf("video") && setIndex(index);
+                }}
+              >
+                {media.contentType.indexOf("video") ? (
+                  <img src={media.src} alt={media.caption} loading="lazy" />
+                ) : (
+                  <video autoPlay controls muted loop>
+                    <source src={media.src} type={media.contentType} />
+                    Your browser does not support the video tag.
+                  </video>
+                )}
+                {media.caption && <ImageListItemBar title={media.caption} />}
+              </ImageListItem>
+            ) : (
+              <Box
+                key={`list-${index}`}
+                sx={{
+                  boxSizing: "border-box",
+                }}
+              >
+                <Skeleton variant="rect" height="100%" />
+              </Box>
+            )
+          )}
+        </ImageList>
+      )}
       {currentMedium && (
         <Lightbox
           mainSrc={currentMedium.src}
